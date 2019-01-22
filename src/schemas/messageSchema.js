6de6cdfb608a81a488bcf4cf03f3a79c888f23ca
Kelly Zegelaar <KNZegelaar@outlook.com>
2019-01-22 15:30:10 +0100

    bugfix

--- src/schemas/messageSchema.js
+++ src/schemas/messageSchema.js
@@ -7,7 +7,7 @@ const MessageSchema = new Schema({
         type: Schema.Types.ObjectId,
         ref: 'user'
     },
-    timeStamp: { type: Date, default: Date.now().toString()},
+    timeStamp: { type: string, default: Date.now().toString()},
     content: String
 });
 
